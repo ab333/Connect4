497539e8133ae8209b0c01e11968cf1189dd34d7
ab333 <ab333@users.noreply.github.com>
2015-03-21 03:35:51 +0300

    Update Connect4.java

--- Connect4.java
+++ Connect4.java
@@ -1,14 +1,23 @@
 
+import java.util.Scanner;
+
+
 public class Connect4 {
 	public static void main(String[] args) {
 		Grid g = new Grid();
-		//g.grid[1][1] = 5;
-		
+		int col;
+		char player = 'X';
 	g.drawBoard();
-	g.refreshBoard(1,'X');
-	g.refreshBoard(1,'O');
-	g.refreshBoard(6,'X');
-	g.drawBoard();
-		//System.out.print(g.grid[1][1]);
+	do {
+		Scanner in = new Scanner(System.in);
+		col = in.nextInt();
+		g.refreshBoard(col,player);
+		g.drawBoard();
+		///// Switch player
+		if(player == 'X')
+			player = 'O';
+		else
+			player = 'X';
+	} while (true);
 	}
 }
