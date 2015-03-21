6f367aeca774b36d42807b7c176d1741a0abe85d
Gensa23 <Gensa23@users.noreply.github.com>
2015-03-21 21:17:03 +0300

    Create IntelligentAgent.java

---
+++ IntelligentAgent.java
@@ -0,0 +1,21 @@
+package connect4;
+public class IntelligentAgent extends Player {
+	private final int horizon = 15;
+	protected Grid tempBoard; 
+	public void startPlaying (Grid board, char player) 
+	{ 
+		super.intelligenceMode=true; 
+		super.startPlaying(board, player);
+	}
+	public char intelligence(Grid board, char player)
+	{	
+		super.intelligence(board, player);
+		board.refreshBoard(alphaBetaSearch(board), player);
+		return 'X'; 
+	}
+	
+	public int alphaBetaSearch (Grid board) {
+	//	TreeNode state = new TreeNode(-1,board,0); 
+	//	int bestMove= searchNode(maxValue(newBoard, -Integer.MAX_VALUE, Integer.MAX_VALUE)).getAction(); 
+		return bestMove; 
+	}
