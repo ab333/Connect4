8ee4663a53bdb8a22ed48c5c525419535fea298b
ab333 <ab333@users.noreply.github.com>
2015-03-21 12:45:46 +0300

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -12,6 +12,9 @@ public class Grid {
 		//grid[1][1] = 5;
 	
 	void drawBoard () {
+		for(int n=0;n<7;n++)
+			System.out.print(" " + n + " ");
+		System.out.println();
 		for(int i=5;i>-1;i--) {
 			for(int j=0;j<grid.length;j++) {
 				System.out.print(" " + grid[j][i] + " ");
@@ -29,6 +32,13 @@ public class Grid {
 				return true;
 			}
 		return false;
+	}
+	boolean boardFull () {
+		for (int i=0;i<cols;i++) {
+			if (grid[i][rows-1] == '_')
+				return false;
+		}
+		return true;
 	}
 	public int rows;
 	public int cols;
