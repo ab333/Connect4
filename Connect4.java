e1ed61a2c2e3c7b6ed37181e2000cdae61f6f465
Gensa23 <Gensa23@users.noreply.github.com>
2015-03-21 20:52:22 +0300

    re-arrange code and add class player
    some improvement of the code and using one function for every case.

--- Connect4.java
+++ Connect4.java
@@ -1,62 +1,27 @@
-import java.util.Random;
 import java.util.Scanner;
-
-
 public class Connect4 {
+	static Grid g = new Grid();
+	static Player user = new Player(); 
 	public static void main(String[] args) {
-		Grid g = new Grid();
-		int col;
-		boolean allowedMove;
 		char player = 'X';
 		System.out.println("Play Against:\n" +
-				"1-Second Player\n" +
-				"2- Random Agent\n" +
-				"3- AI Agent");
+								"1- Another Player\n " +
+								"2- Random Agent\n" +
+								"3- AI Agent");
+		
 		Scanner in = new Scanner(System.in);
 		int option = in.nextInt();
 		g.drawBoard();
 		if(option == 1) {
-			do {
-				do {
-					in = new Scanner(System.in);
-					col = in.nextInt();
-					allowedMove = g.refreshBoard(col,player);
-				} while (!allowedMove); // Add Referee isAllowed here 
-				g.drawBoard();
-				///// Switch players
-				if(player == 'X')
-					player = 'O';
-				else
-					player = 'X';
-			} while (!g.boardFull());
-			
+			user.startPlaying(g, player);
 		}
 		else if(option == 2) {
-			do {
-				if(player == 'X') {
-					do {
-						in = new Scanner(System.in);
-						col = in.nextInt();
-						allowedMove = g.refreshBoard(col,player);
-					} while (!allowedMove); // Add Referee isAllowed here
-					player = 'O';
-				}
-				else {
-					do {
-						Random rand = new Random();
-						col = rand.nextInt((6 - 0) + 1) + 0;
-						allowedMove = g.refreshBoard(col,player);
-					} while (!allowedMove); // Add Referee isAllowed here
-					player = 'X';
-				}
-					
-				g.drawBoard();
-				
-				
-			} while (!g.boardFull());
+			user.activateRandomPlayer();
+			user.startPlaying(g, player);
 		}
 		else if(option == 3) {
-			// TODO
+			IntelligentAgent Ai = new IntelligentAgent (); 
+			Ai.startPlaying(g, player);
 		}
 		in.close();
 	}
