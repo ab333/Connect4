78264dc1b51e52f6118505c819df3e0027a97773
Gensa23 <Gensa23@users.noreply.github.com>
2015-03-25 15:21:49 +0300

    agent vs agent option is added

--- Connect4.java
+++ Connect4.java
@@ -1,19 +1,21 @@
+package connect4;
 import java.util.Scanner;
 public class Connect4 {
 	static Grid g = new Grid();
 	static Player user = new Player(); 
 	public static void main(String[] args) {
-		char player = 'X';
+		char player = 'X'; 
 		System.out.println("Play Against:\n" +
-								"1- Another Player\n " +
+								"1- Another Player\n" +
 								"2- Random Agent\n" +
-								"3- AI Agent");
+								"3- AI Agent\n" +
+								"4- Agent vs Agent\n");
 		
 		Scanner in = new Scanner(System.in);
 		int option = in.nextInt();
 		g.drawBoard();
 		if(option == 1) {
-			user.startPlaying(g, player);
+			user.startPlaying(g, player);  
 		}
 		else if(option == 2) {
 			user.activateRandomPlayer();
@@ -22,6 +24,12 @@ public class Connect4 {
 		else if(option == 3) {
 			IntelligentAgent Ai = new IntelligentAgent (); 
 			Ai.startPlaying(g, player);
+		}
+		else if(option == 4)
+		{
+			IntelligentAgent smart = new IntelligentAgent ();
+			smart.activateAgentVsAgent();
+			smart.startPlaying(g, player);
 		}
 		in.close();
 	}
